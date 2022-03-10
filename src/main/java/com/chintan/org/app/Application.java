4646fd918b5d4f2015f93d69f5cd175212cc22bd
Chintan <chintanmahadik@gmail.com>
2022-03-10 22:12:37 +0530

    env Config added

--- src/main/java/com/chintan/org/app/Application.java
+++ src/main/java/com/chintan/org/app/Application.java
@@ -2,10 +2,6 @@ package com.chintan.org.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication()
 public class Application {
